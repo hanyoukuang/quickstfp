2727883161df2275b8f420463a9476a30f25ae5d
hanyoukuang <161329230+hanyoukuang@users.noreply.github.com>
2025-09-12 20:11:12 +0800

    Add files via upload

---
+++ user_database.py
@@ -0,0 +1,57 @@
+import sqlite3
+
+
+class UserInfoData:
+    def __init__(self):
+        self.conn = sqlite3.connect('host.db')
+        self.cursor = self.conn.cursor()
+        self.create_table()
+
+    def create_table(self) -> None:
+        self.cursor.execute('''CREATE TABLE IF NOT EXISTS Host
+                               (
+                                   id
+                                   INTEGER
+                                   PRIMARY
+                                   KEY,
+                                   host
+                                   TEXT,
+                                   port
+                                   INTEGER,
+                                   username
+                                   TEXT,
+                                   password
+                                   TEXT
+                               )''')
+
+    def query(self, host, port, username, password) -> list:
+        self.cursor.execute(f'SELECT * FROM Host where host = ? AND port = ? AND username = ? AND password = ?', (host,
+                                                                                                                  port,
+                                                                                                                  username,
+                                                                                                                  password))
+        return self.cursor.fetchall()
+
+    def insert(self, host, port, username, password) -> None:
+        if len(self.query(host, port, username, password)):
+            return
+        self.cursor.execute(f'INSERT INTO Host (host, port, username, password) VALUES (?, ?, ?, ?)',
+                            (host, port, username, password))
+        self.conn.commit()
+
+    def query_all(self):
+        self.cursor.execute('SELECT * FROM Host')
+        return self.cursor.fetchall()
+
+    def query_idx(self, idx: int):
+        self.cursor.execute('SELECT * FROM Host WHERE id = ?', (idx,))
+        return self.cursor.fetchone()
+
+    def del_idx(self, idx: int):
+        self.cursor.execute('DELETE FROM Host WHERE id = ?', (idx,))
+        self.conn.commit()
+
+
+if __name__ == '__main__':
+    userinfo = UserInfoData()
+    userinfo.insert('localhost', 5432, 'admin', 'admin')
+    print(userinfo.query_all())
